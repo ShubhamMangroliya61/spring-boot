5a31eaafbe899b4ffe39bec2da11ce05151000af
Aditya Kaneriya <adityakaneriya1711@gmail.com>
2024-05-07 14:02:53 +0530

    [Implement]: CircuitBreaker for fault tolerance using resilience4j

--- micro-servoces-durgesh/user-service/src/main/java/com/microservices/userservice/controller/UserController.java
+++ micro-servoces-durgesh/user-service/src/main/java/com/microservices/userservice/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.microservices.userservice.controller;
 
 import com.microservices.userservice.entities.User;
+import com.microservices.userservice.response.ApiResponse;
 import com.microservices.userservice.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,14 +30,23 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<?> getUser(@PathVariable String userId){
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
     }
 
     @GetMapping("/getAll")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<?> getAllUsers(){
         List<User> listOfUsers = userService.getAllUsers();
         return ResponseEntity.ok(listOfUsers);
+    }
+
+    // Fallback method for service breaker.
+    public ResponseEntity<?> ratingHotelFallback(Exception exception){
+        System.out.println("Failed..........");
+        ApiResponse response = new ApiResponse("Some related service is down..", false, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
